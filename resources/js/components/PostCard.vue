fd0be005b36c64c4adbb4104c80196272c707e14
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-30 17:43:08 +0200

    feat: :sparkles: New PostCard layout and information

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -3,15 +3,29 @@
         <div class="row">
             <div class="col-12">
                 <div class="card mt-3">
-                    <div class="card-title">
-                        {{ post.title }}
+
+
+                    <div class="card text-center" :style=" `border: 3px solid ${post.category.color} ;`">
+                        <div class="card-header" :style=" `border-bottom: 3px solid ${post.category.color} ;`">
+                            <router-link class="nav-link" :to="`/posts/${post.id}`">
+                                {{ post.title }}
+                            </router-link>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title text-center">
+                                <img :src="post.post_image" :alt="post.title" >
+                            </div>
+                            <p class="card-text">{{ post.post_content }}</p>
+                        </div>
+                        <footer class="blockquote-footer"> {{ post.user.name }} <cite title="Source Title">{{ post.post_date }}</cite> | Category:
+                            <router-link class="nav-link" :to="`/categories/${post.category.id}`">
+                                {{ post.category.name }}
+                            </router-link>
+                        </footer>
+
                     </div>
-                    <div class="card-title">
-                        <img :src="post.post_image" :alt="post.title" >
-                    </div>
-                    <div class="card-content">
-                        {{ post.post_content }}
-                    </div>
+
+
                 </div>
             </div>
         </div>
@@ -21,7 +35,9 @@
 <script>
     export default {
         name : 'PostCard',
-        props:['post']
+        props:{
+            post:Object,
+        }
     };
 </script>
 
