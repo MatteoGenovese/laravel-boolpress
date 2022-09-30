2662f379d103ce017ae15eefd5813654e13b1f71
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-30 17:43:51 +0200

    style: :sparkles: new layout and new bottom for search

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -3,9 +3,23 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="p-3 m-3">
-                        Recent posts:
-                    </h1>
+                    <div class="row">
+                        <div class="col-6">
+                            <h1 class="p-3 m-3">
+                                Recent posts:
+                            </h1>
+                        </div>
+                        <div class="col-6 ">
+                            <nav class="navbar navbar-light bg-light d-flex justify-content-end">
+                                <form class="form-inline d-flex justify-content-end">
+                                    <input class="form-control mr-sm-2 d-flex justify-content-end" type="search" placeholder="Search title" aria-label="Search">
+                                    <button class="btn btn-outline-success my-2 my-sm-0 d-flex justify-content-end" type="submit">Search</button>
+                                </form>
+                            </nav>
+
+                        </div>
+
+                    </div>
                     <PostCard v-for="post in posts" :key="post.id" :post="post"/>
                 </div>
             </div>
